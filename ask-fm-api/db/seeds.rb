8ab7a37cad8c03080617c5f955deffe5031a610c
Veronica <veronica.gudova@gmail.com>
2021-04-03 15:25:14 -0700

    seeded db

--- ask-fm-api/db/seeds.rb
+++ ask-fm-api/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.delete_all
+Answer.delete_all
+Comment.delete_all
+
+num_q=10
+num_ac=5
+
+num_q.times do
+    created_at=Faker::Date.backward(days: 365)
+    q=Question.create(
+        title: Faker::GreekPhilosophers.unique.quote,
+        created_at: created_at
+    )
+    if q.valid?
+        q.answers = num_ac.times.map do
+            a=Answer.new(
+                title: Faker::Lorem.unique.sentence
+            )
+        end
+        q.answers.each do |answer|
+            answer.comments= num_ac.times.map do
+                Comment.new(
+                    title: Faker::Lorem.unique.sentence
+                )
+            end
+        end
+    end
+end
+
+puts "Created #{Question.count} questions, #{Answer.count} answers, #{Comment.count} comments"
